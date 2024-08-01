9076770a7ebe0aac8fbd91fac2c66f69d81332a1
julcontrerass <111258374+julcontrerass@users.noreply.github.com>
2024-08-01 17:21:39 -0300

    edit error of navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,7 @@ const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
-}
-
-
+};
 
 /**
  * PRELOADER
@@ -21,8 +19,6 @@ window.addEventListener("DOMContentLoaded", function () {
   document.body.classList.add("loaded");
 });
 
-
-
 /**
  * NAVBAR
  * navbar toggle for mobile
@@ -32,17 +28,28 @@ const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
 const navbar = document.querySelector("[data-navbar]");
 const overlay = document.querySelector("[data-overlay]");
+const navbarLinks = document.querySelectorAll(".navbar-link");
 
 const toggleNavbar = function () {
   navbar.classList.toggle("active");
   navToggleBtn.classList.toggle("active");
   overlay.classList.toggle("active");
   document.body.classList.toggle("nav-active");
-}
+};
 
-addEventOnElements(navTogglers, "click", toggleNavbar);
+// Add click event to navbar togglers
+navTogglers.forEach((toggler) => {
+  toggler.addEventListener("click", toggleNavbar);
+});
 
-
+// Add click event to navbar links
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    if (navbar.classList.contains("active")) {
+      toggleNavbar();
+    }
+  });
+});
 
 /**
  * HEADER
@@ -59,8 +66,6 @@ window.addEventListener("scroll", function () {
   }
 });
 
-
-
 /**
  * SLIDER
  */
@@ -68,19 +73,23 @@ window.addEventListener("scroll", function () {
 const sliders = document.querySelectorAll("[data-slider]");
 
 const initSlider = function (currentSlider) {
-
-  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
+  const sliderContainer = currentSlider.querySelector(
+    "[data-slider-container]"
+  );
   const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
   const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
 
-  let totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
-  let totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+  let totalSliderVisibleItems = Number(
+    getComputedStyle(currentSlider).getPropertyValue("--slider-items")
+  );
+  let totalSlidableItems =
+    sliderContainer.childElementCount - totalSliderVisibleItems;
 
   let currentSlidePos = 0;
 
   const moveSliderItem = function () {
     sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
-  }
+  };
 
   /**
    * NEXT SLIDE
@@ -95,7 +104,7 @@ const initSlider = function (currentSlider) {
     }
 
     moveSliderItem();
-  }
+  };
 
   sliderNextBtn.addEventListener("click", slideNext);
 
@@ -110,14 +119,14 @@ const initSlider = function (currentSlider) {
     }
 
     moveSliderItem();
-  }
+  };
 
   sliderPrevBtn.addEventListener("click", slidePrev);
 
   const dontHaveExtraItem = totalSlidableItems <= 0;
   if (dontHaveExtraItem) {
-    sliderNextBtn.style.display = 'none';
-    sliderPrevBtn.style.display = 'none';
+    sliderNextBtn.style.display = "none";
+    sliderPrevBtn.style.display = "none";
   }
 
   /**
@@ -134,48 +143,99 @@ const initSlider = function (currentSlider) {
    */
 
   window.addEventListener("resize", function () {
-    totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-items"));
-    totalSlidableItems = sliderContainer.childElementCount - totalSliderVisibleItems;
+    totalSliderVisibleItems = Number(
+      getComputedStyle(currentSlider).getPropertyValue("--slider-items")
+    );
+    totalSlidableItems =
+      sliderContainer.childElementCount - totalSliderVisibleItems;
 
     moveSliderItem();
   });
+};
 
+for (let i = 0, len = sliders.length; i < len; i++) {
+  initSlider(sliders[i]);
 }
-
-for (let i = 0, len = sliders.length; i < len; i++) { initSlider(sliders[i]); }
 
 
 /**
- * aviso de mail 
- */
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.getElementById("contactForm");
+   * aviso de mail en espanol
+   */
+document.getElementById('contactForm').addEventListener('submit', async function(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
 
-  // Mensajes en diferentes idiomas
-  const messages = {
-    en: {
-      success: "Your message has been sent successfully!",
-      error: "There was an error sending your message. Please try again.",
-    },
-    es: {
-      success: "¡Tu mensaje ha sido enviado exitosamente!",
-      error: "Hubo un error al enviar tu mensaje. Por favor, intenta de nuevo.",
-    },
-  };
+  Swal.fire({
+    title: 'Enviando...',
+    text: 'Por favor, espera un momento.',
+    allowOutsideClick: false,
+    didOpen: () => {
+      Swal.showLoading();
+    }
+  });
 
-  // Detectar el idioma del navegador
-  const userLang = navigator.language || navigator.userLanguage;
-  const lang = userLang.startsWith("es") ? "es" : "en"; // Asume inglés por defecto
+  try {
+    const response = await fetch(form.action, {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
 
-  // Evento de envío del formulario
-  form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Previene el envío por defecto
+    if (response.ok) {
+      Swal.fire({
+        title: '¡Éxito!',
+        text: 'Mensaje enviado correctamente',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+      });
+      form.reset();
+    } else {
+      throw new Error('Error en el envío del formulario');
+    }
+  } catch (error) {
+    Swal.fire({
+      title: 'Error',
+      text: 'Hubo un error al enviar el mensaje. Por favor, intenta de nuevo.',
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    });
+  }
+});
 
-    fetch(form.action, {
-      method: form.method,
-      body: new FormData(form),
-    })
-      .then((response) => {
+/**
+ * aviso de mail en espanol
+*/
+ document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+
+    // Mensajes en diferentes idiomas
+    const messages = {
+      en: {
+        success: 'Your message has been sent successfully!',
+        error: 'There was an error sending your message. Please try again.'
+      },
+      es: {
+        success: '¡Tu mensaje ha sido enviado exitosamente!',
+        error: 'Hubo un error al enviar tu mensaje. Por favor, intenta de nuevo.'
+      }
+    };
+
+    // Detectar el idioma del navegador
+    const userLang = navigator.language || navigator.userLanguage;
+    const lang = userLang.startsWith('es') ? 'es' : 'en'; // Asume inglés por defecto
+
+    // Evento de envío del formulario
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Previene el envío por defecto
+
+      fetch(form.action, {
+        method: form.method,
+        body: new FormData(form),
+      })
+      .then(response => {
         if (response.ok) {
           alert(messages[lang].success); // Muestra mensaje de éxito en el idioma correspondiente
         } else {
@@ -185,26 +245,28 @@ document.addEventListener("DOMContentLoaded", function () {
       .catch(() => {
         alert(messages[lang].error); // Muestra mensaje de error en caso de excepción
       });
+    });
   });
+
+/**
+ *  ScrollReveal
+*/
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  ScrollReveal().reveal('.blog-card', {
+    duration: 1000,  // Duración de la animación en milisegundos
+    origin: 'bottom',  // Desde dónde viene la animación (top, bottom, left, right)
+    distance: '50px',  // Distancia que recorrerá el elemento
+    delay: 200,  // Retraso antes de que la animación comience
+    reset: true  // Si deseas que la animación se repita al hacer scroll
 });
-
-// /**
-//  *  ScrollReveal
-//  */
-
-document.addEventListener("DOMContentLoaded", function () {
-  ScrollReveal().reveal(".blog-card", {
-    duration: 1000, // Duración de la animación en milisegundos
-    origin: "bottom", // Desde dónde viene la animación (top, bottom, left, right)
-    distance: "50px", // Distancia que recorrerá el elemento
-    delay: 200, // Retraso antes de que la animación comience
-    reset: true, // Si deseas que la animación se repita al hacer scroll
-  });
-  ScrollReveal().reveal(".reveal", {
-    duration: 1000,
-    origin: "top",
-    distance: "50px",
-    delay: 200,
-    reset: true,
+  
+  ScrollReveal().reveal('.reveal', {
+      duration: 1000,
+      origin: 'top',
+      distance: '50px',
+      delay: 200,
+      reset: true
   });
 });
