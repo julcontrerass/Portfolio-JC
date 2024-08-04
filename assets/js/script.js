be9c8a0183833a8b27203e0df5a9948a30e4d2bb
julcontrerass <111258374+julcontrerass@users.noreply.github.com>
2024-08-04 19:22:52 -0300

    edit Web Analytics

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 /**
+ * Web Analytics
+ */
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://cdn.vercel-insights.com/v1/script.js";
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"vercel","script");
+vercel("init", { projectId: "juliancontreras-portfolio.vercel.app" });
+
+/**
  * add event listener on multiple elements
  */
 
